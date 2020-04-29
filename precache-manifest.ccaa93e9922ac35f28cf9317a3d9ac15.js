c35adc1cff874699a2861e83087026c8decfab83
Tatyana Pivgorodnyaya <peachpeachttt@gmail.com>
2020-04-29 17:29:15 +0300

    Updates

---
+++ precache-manifest.ccaa93e9922ac35f28cf9317a3d9ac15.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba67b3ba1e25944a9bbe07717f80f49d",
+    "url": "/CatsFood/index.html"
+  },
+  {
+    "revision": "085770a466e896220d78",
+    "url": "/CatsFood/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a9c841db8c8998258e20",
+    "url": "/CatsFood/static/css/main.94478541.chunk.css"
+  },
+  {
+    "revision": "085770a466e896220d78",
+    "url": "/CatsFood/static/js/2.ad838a0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CatsFood/static/js/2.ad838a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c841db8c8998258e20",
+    "url": "/CatsFood/static/js/main.592bafef.chunk.js"
+  },
+  {
+    "revision": "9fb10c2cab1a4f29af3a",
+    "url": "/CatsFood/static/js/runtime-main.7a9e302d.js"
+  },
+  {
+    "revision": "66c7f1c025003b125d95c85a6d1f748c",
+    "url": "/CatsFood/static/media/Exo20-Thin.66c7f1c0.ttf"
+  },
+  {
+    "revision": "7b435128f4aa5d3f6202e35a339643c0",
+    "url": "/CatsFood/static/media/Exo20-Thin.7b435128.woff2"
+  },
+  {
+    "revision": "b7f2073db20fbe66e67c7b28189c9f3b",
+    "url": "/CatsFood/static/media/Exo20-Thin.b7f2073d.woff"
+  },
+  {
+    "revision": "d7c6be94af1ccf70fc92eff7cdade01b",
+    "url": "/CatsFood/static/media/Exo20-Thin.d7c6be94.eot"
+  },
+  {
+    "revision": "f853f151af509e451454e3a735e84ae4",
+    "url": "/CatsFood/static/media/back.f853f151.png"
+  },
+  {
+    "revision": "53182a43c5e1cd6ea9ed2c04da46cc30",
+    "url": "/CatsFood/static/media/cat.53182a43.png"
+  }
+]);
